03ebfb6d1d802fdce9cb9ca53713199cb443ff92
Tate Rathje <taterathje@gmail.com>
2014-07-16 19:41:56 -0700

    Fixed delete function in Todos factory.

--- public/js/services/todos.js
+++ public/js/services/todos.js
@@ -11,7 +11,7 @@ angular.module('todoService', [])
 				return $http.post('/api/todos', todoData);
 			},
 			delete : function(id) {
-				return $http.delete('/api/todos' + id);
+				return $http.delete('/api/todos/' + id);
 			}
 		}
 	});
